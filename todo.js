e8e609b7f4488f597308f51a3e95f7745fe0b1b9
Park Na Yeon <within197@naver.com>
2021-02-14 23:45:49 +0900

    todo

---
+++ todo.js
@@ -0,0 +1,61 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+// LS = localStorage
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  //value에 저장할 때는 list->string으로 바꿔서~
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  span.innerText = text;
+
+  toDoList.appendChild(li);
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); //event를 작동하지 못하게 하는 메서드
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  //localStorage 저장이 이미 되있거나 안되있거나
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    //출력할때는 string->list로 바꿔서
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
+    //JSON javascript object notaiton
+  } else {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
